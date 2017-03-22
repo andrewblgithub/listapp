376a0a633c817ca6a7a473773cd95436a3ada032
Andrew Lichtenstein <andrewbl@icloud.com>
2017-03-22 18:50:44 +0000

    Added bootstrap with SASS gem. Also implemented global navigation bar.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
+  #whitelist config for error: cannot render console from ips"
+  config.web_console.whitelisted_ips = '208.78.25.221'
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
