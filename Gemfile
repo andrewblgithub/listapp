4357f2d87637e51916b1f60658c454ba8cb19142
Andrew Lichtenstein <andrewbl@icloud.com>
2017-03-27 15:44:05 +0000

    Styled home page post panels and post form. Added datetimepicker for later usage

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
+# datetimepicker for form
+gem 'momentjs-rails', '>= 2.9.0'
+gem 'bootstrap3-datetimepicker-rails', '~> 4.17.47'
 # Devise gem for authentication and user accounts
 gem 'devise'
 # for UI components including navbar
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323180449) do
+ActiveRecord::Schema.define(version: 20170325045037) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
