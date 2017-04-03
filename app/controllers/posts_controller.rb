395b010a7cb89eed5bc43aafc58b3094dd07e5d5
Andrew Lichtenstein <andrewbl@icloud.com>
2017-04-03 21:04:42 +0000

    Improved posts structure with index, show, new but still WIP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,24 @@
 class PostsController < ApplicationController
+    
+    def index
+        @newPost = Post.new
+        @posts = Post.all
+    end
         
     def show
+        @newPost = Post.new
         @post=Post.find(params[:id])
     end
     
     def new
-        @post = Post.new
+        @newPost = Post.new
     end
     
     def create
         @post = Post.new(post_params)
         @post.user_id = current_user.id # assign post to user who created it
-        respond_to do |f|
-            if (@post.save)
-                f.html { redirect_to :back, notice: "Post created!" }
-            else
-                f.html { redirect_to :back, notice: "Error: post not saved!" }
-            end
-        end
+        @post.save
+        redirect to :back
     end
     
     def destroy
